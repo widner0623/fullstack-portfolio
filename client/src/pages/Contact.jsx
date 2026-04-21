d13089f0af5eb8022ad663a768b2c2f9856b4ea8
Derrick Widner <derrick.widner@outlook.com>
2026-04-21 09:59:53 -0500

    made a marque track for contact page giving users a little bit of info to inspire them to contact me

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,7 +3,24 @@ import "../styles/contact.css";
 function Contact() {
     return (
         <div className="contact">
-            <h1>In Development...</h1>
+            <h1>Contact Me</h1>
+
+            <div className="marquee">
+                <div className="marquee-track">
+                    <span>
+                        Let’s turn your idea into something real • Ready to start your project? • Let’s build something that stands out • Your vision deserves to be built • Let’s get to work •
+                    </span>
+                    <span>
+                        Need a website that actually works for your business? • Built for performance, speed, and results • Clean design that converts visitors into customers • Let’s solve real problems •
+                    </span>
+                   <span>
+                        Let’s turn your idea into something real • Ready to start your project? • Let’s build something that stands out • Your vision deserves to be built • Let’s get to work •
+                    </span>
+                    <span>
+                        Need a website that actually works for your business? • Built for performance, speed, and results • Clean design that converts visitors into customers • Let’s solve real problems •
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
