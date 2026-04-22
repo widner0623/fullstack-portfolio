1e134430df09c1e9d75d714c3484dd7cc38b8b71
Derrick Widner <derrick.widner@outlook.com>
2026-04-21 22:08:56 -0500

    changed form from my email to business email

--- server/index.js
+++ server/index.js
@@ -40,9 +40,7 @@ const Contact = mongoose.model("Contact", contactSchema);
 
 /* mail */
 const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false,
+    host: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
